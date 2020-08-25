20e8469e0b5908b7dbadf862ac83cf645767bf9b
Takashi Umeno <umeno.takashi@gmail.com>
2020-08-25 22:47:46 +0900

    change: add LINE Notify
    send_info(msg)
    Reference:
    https://qiita.com/sunleth/items/bc9fa61866d6f23b3a18

--- get_flows/get_flows_lambda.py
+++ get_flows/get_flows_lambda.py
@@ -3,23 +3,44 @@ import os
 import sys
 import uuid
 from urllib.parse import unquote_plus
+import urllib.request
 
 import requests
 import json
 import datetime
 
+# LINE notify's API
+LINE_TOKEN = os.environ['LINE_TOKEN']
+LINE_NOTIFY_URL = "https://notify-api.line.me/api/notify"
+line_msg = os.environ['LINE_MSG']
+
+# Read environment variable
+stealthwatch_cloud_portal_url = os.environ['STEALTHWATCH_CLOUD_PORTAL_URL']
+stealthwatch_cloud_api_user = os.environ['STEALTHWATCH_CLOUD_API_USER']
+stealthwatch_cloud_api_key = os.environ['STEALTHWATCH_CLOUD_API_KEY']
+stealthwatch_cloud_minites = int(os.environ['STEALTHWATCH_CLOUD_MINITES'])
+stealthwatch_cloud_min_flows = int(os.environ['STEALTHWATCH_CLOUD_MIN_FLOWS'])
+
 s3_client = boto3.client('s3')
+
+def send_info(msg):
+    method = "POST"
+    headers = {"Authorization": "Bearer %s" % LINE_TOKEN}
+    payload = {"message": msg}
+    try:
+        payload = urllib.parse.urlencode(payload).encode("utf-8")
+        req = urllib.request.Request(
+            url=LINE_NOTIFY_URL, data=payload, method=method, headers=headers)
+        urllib.request.urlopen(req)
+    except Exception as e:
+        print ("Exception Error: ", e)
+        sys.exit(1)
 
 def get_flows(upload_path):
     try:
         requests.packages.urllib3.disable_warnings()
     except:
         pass
-
-    # Read environment variable
-    stealthwatch_cloud_portal_url = os.environ['STEALTHWATCH_CLOUD_PORTAL_URL']
-    stealthwatch_cloud_api_user = os.environ['STEALTHWATCH_CLOUD_API_USER']
-    stealthwatch_cloud_api_key = os.environ['STEALTHWATCH_CLOUD_API_KEY']
 
     # Set the URL
     url = "https://" + stealthwatch_cloud_portal_url + "/api/v3/snapshots/session-data/"
@@ -36,7 +57,7 @@ def get_flows(upload_path):
 
     # Set the timestamps for the filters, in the correct format, for last 60 minutes
     end_datetime = datetime.datetime.utcnow()
-    start_datetime = end_datetime - datetime.timedelta(minutes=60)
+    start_datetime = end_datetime - datetime.timedelta(minutes=stealthwatch_cloud_minites)
     end_timestamp = end_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
     start_timestamp = start_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
 
@@ -63,6 +84,13 @@ def get_flows(upload_path):
         # write file
         with open(upload_path, 'w') as f:
             json.dump(flows, f, indent=4)
+
+        # print len json
+        flows_len = len(flows)
+        print('flows_len = %d, stealthwatch_cloud_min_flows = %d' % (flows_len, stealthwatch_cloud_min_flows))
+        if (flows_len <= stealthwatch_cloud_min_flows):
+            print('flows_len <= stealthwatch_cloud_min_flows, line_msg: %s' % line_msg)
+            send_info(line_msg)
 
     # If unable to fetch list of alerts
     else:
